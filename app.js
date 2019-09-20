8203bc2d3094702fb013c527d319a4354b97d8cc
unknown <pedrorf27@hotmail.com>
2019-09-20 13:38:11 -0300

    update dburl

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
 
-mongoose.connect(process.env.DATABASEURL,  { useNewUrlParser: true, useUnifiedTopology: true });
-//mongoose.connect("mongodb+srv://pedrorf27:enem362880@clothesproject-1dbck.mongodb.net/test?retryWrites=true&w=majority",   { useNewUrlParser: true, useUnifiedTopology: true });
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp";
+mongoose.connect(url,  { useNewUrlParser: true, useUnifiedTopology: true });
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
