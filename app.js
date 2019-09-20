8919c7b0458ddbe466a4391310bd265413a78379
unknown <pedrorf27@hotmail.com>
2019-09-20 09:54:22 -0300

    add mongodb.atlas

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp",  { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect("mongodb://localhost/yelp_camp",  { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://pedrorf27:enem362880@clothesproject-1dbck.mongodb.net/test?retryWrites=true&w=majority");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
